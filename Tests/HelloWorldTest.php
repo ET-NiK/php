a2735117f8c18c5f687f4c3ce7661cd3bb3f47f7
Nikita <kronstadtsky@bk.ru>
2015-03-16 00:41:18 +0300

    Update HelloWorldTest.php

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -9,6 +9,26 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $strServer = "localhost";
+        $strServerPort = "21";
+        $strServerUsername = "root";
+        $strServerPassword = "";
+        
+        //connect to server
+        $resConnection = ssh2_connect($strServer, $strServerPort);
+        
+        if(ssh2_auth_password($resConnection, $strServerUsername, $strServerPassword)){
+        	//Initialize SFTP subsystem
+        	$resSFTP = ssh2_sftp($resConnection);
+        	
+        	//
+        	//Send/Download file here
+        	//
+        }else{
+        	echo "Unable to authenticate on server";
+        }
+
+
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
